00122bdf21cbd8d031a662e2314ee3edd6e0614c
Krish Sanghvi <124244390+Krishsanghvii@users.noreply.github.com>
2023-08-02 23:02:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ with st.sidebar:
     menu = ['Amazon Review Sentiment Analysis','Product Queries BOT']
     choice  = st.sidebar.selectbox("Select an option", menu)
     add_vertical_space(10)
-    st.write('Made by [Rohit Wahwal](https://github.com/zerothrohit)')
+    st.write('Made by [Krish Sanghvi](https://github.com/Krishsanghvii)')
 
 st.header("Your Amazon Assistant 💬")
 st.divider()
